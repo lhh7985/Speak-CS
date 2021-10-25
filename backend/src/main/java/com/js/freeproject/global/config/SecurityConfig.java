7cac45218693f7beacb32a5871320ff5b17b1699
서정욱 <junguk7880@naver.com>
2021-10-25 08:23:57 +0000

    [BE] feat: Add user service

---
+++ backend/src/main/java/com/js/freeproject/global/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.js.freeproject.global.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.js.freeproject.domain.user.appliction.UserService;
+import com.js.freeproject.global.jwt.JwtFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private UserService userService;
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/h2-console/**","/swagger-resources/**","/swagger-ui.html","/swagger/**");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable()
+			.addFilter(new JwtFilter(authenticationManager(),userService))
+			.authorizeRequests()
+			.antMatchers("/user/me").authenticated()
+			.antMatchers("/user/**").permitAll()
+			.anyRequest().permitAll()
+			.and()
+			.formLogin()
+			.and()
+			.httpBasic()
+			.and()
+			.cors();
+	}
+	
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
