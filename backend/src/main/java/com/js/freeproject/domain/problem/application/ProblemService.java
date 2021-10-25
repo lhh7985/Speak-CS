737e7be7892dde9970ff9858f8be6a3dc4466a73
KingBlackCow <sgs1159@naver.com>
2021-10-25 16:32:03 +0900

    refactor : 에러처리 exhandler 화

--- backend/src/main/java/com/js/freeproject/domain/problem/application/ProblemService.java
+++ backend/src/main/java/com/js/freeproject/domain/problem/application/ProblemService.java
@@ -30,9 +30,9 @@ public class ProblemService {
     @Transactional
     public Long saveProblem(ProblemMakeRequest problemMakeRequest) {
         User user = userRepository.findById(problemMakeRequest.getUserId())
-                .orElseThrow(() -> new IllegalStateException("유저가 존재하지 않습니다."));
+                .orElseThrow(() -> new IllegalArgumentException("유저가 존재하지 않습니다."));
         Category category = categoryRepository.findById(problemMakeRequest.getCategoryId())
-                .orElseThrow(() -> new IllegalStateException("해당 카테고리가 존재하지 않습니다."));
+                .orElseThrow(() -> new IllegalArgumentException("해당 카테고리가 존재하지 않습니다."));
         List<Answer> answers =
                 problemMakeRequest.getAnswers().stream().map(Answer::new).collect(toList());
         Problem newProblem = Problem.createProblem(
@@ -44,7 +44,7 @@ public class ProblemService {
         try {
             problemRepository.save(newProblem);
         }catch (Exception e){
-            log.info(e.getMessage());
+            throw new IllegalStateException(e.getMessage());
         }
         return newProblem.getId();
     }
@@ -74,9 +74,15 @@ public class ProblemService {
 
     public AnswerResponse findAnswer(Long id, String myAns) {
         Optional<Problem> problems = problemRepository.findById(id);
-        problems.orElseThrow(() -> new IllegalStateException("문제에 답을 찾을 수 없습니다."));
+        problems.orElseThrow(() -> new IllegalArgumentException("문제에 답을 찾을 수 없습니다."));
         List<String> proper = new ArrayList<>();
         List<String> wrong = new ArrayList<>();
+        extractedAnswer(myAns, problems, proper, wrong);
+        AnswerResponse answerResponse = new AnswerResponse(proper, wrong);
+        return answerResponse;
+    }
+
+    private void extractedAnswer(String myAns, Optional<Problem> problems, List<String> proper, List<String> wrong) {
         for (Answer answer : problems.get().getAnswers()) {
             if (myAns.contains(answer.getWord())) {
                 proper.add(answer.getWord());
@@ -84,17 +90,14 @@ public class ProblemService {
                 wrong.add(answer.getWord());
             }
         }
-        AnswerResponse answerResponse = new AnswerResponse(proper, wrong);
-        return answerResponse;
     }
 
     @Transactional
     public String updateStatus(Long id, ProblemStatus problemStatus) {
-        Optional<Problem> problem = problemRepository.findById(id);
-        if (!problem.isPresent()) {
-            return "문제가 존재하지 않습니다.";
-        }
-        problem.get().setStatus(problemStatus);
+        Problem problem = problemRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("문제가 존재하지 않습니다."));
+
+        problem.setStatus(problemStatus);
         return "변경이 완료되었습니다.";
     }
 }
