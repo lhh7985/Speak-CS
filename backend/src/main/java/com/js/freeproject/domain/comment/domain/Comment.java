fe708850db390d7ff5d49e157698262e6d321efb
hohwang <ghghkd52@naver.com>
2021-10-26 14:39:22 +0900

    [BE] refactor: Comment Entity 리팩토링

--- backend/src/main/java/com/js/freeproject/domain/comment/domain/Comment.java
+++ backend/src/main/java/com/js/freeproject/domain/comment/domain/Comment.java
@@ -1,36 +1,59 @@
 package com.js.freeproject.domain.comment.domain;
 
-import java.util.List;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.js.freeproject.domain.board.domain.Board;
-
+import com.js.freeproject.domain.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String content;
-	
-	@ManyToOne
+
+	private LocalDateTime createdAt;
+
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	@JoinColumn(name="board_id")
 	private Board board;
-	
-	@ManyToOne
+
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	@JoinColumn(name="parent_id")
-	private Comment comment;
+	private Comment parent;
 	
-	@OneToMany(mappedBy = "comment")
-	private List<Comment> comments;
+//	@OneToMany(mappedBy = "comment", cascade = CascadeType.ALL)
+//	private List<Comment> comments;
+
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@Builder
+	public Comment(String content, Board board, Comment parent, User user) {
+		this.content = content;
+		this.board = board;
+		this.parent = parent;
+		this.user = user;
+	}
+
+	public Comment updateParent(final Comment comment){
+		this.parent = comment;
+		return this;
+	}
+
+	@PrePersist
+	public void createdAt() {
+		this.createdAt = LocalDateTime.now();
+	}
 }
