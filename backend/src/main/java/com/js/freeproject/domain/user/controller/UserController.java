da8e843884354989277b4eed84417f124e755590
Seojeonguk <uk7880@naver.com>
2021-10-26 17:22:16 +0900

    [BE] feat: Create swagger

--- backend/src/main/java/com/js/freeproject/domain/user/controller/UserController.java
+++ backend/src/main/java/com/js/freeproject/domain/user/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.js.freeproject.domain.user.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -12,20 +10,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.js.freeproject.domain.model.CommonResponse;
 import com.js.freeproject.domain.user.appliction.UserService;
 import com.js.freeproject.domain.user.domain.User;
 import com.js.freeproject.domain.user.dto.LoginRequest;
+import com.js.freeproject.domain.user.dto.LoginResponse;
 import com.js.freeproject.domain.user.dto.UserRequest;
 import com.js.freeproject.domain.user.dto.UserResponse;
 import com.js.freeproject.global.jwt.CustomUserDetails;
 import com.js.freeproject.global.jwt.TokenProvider;
 import com.sun.jdi.request.DuplicateRequestException;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/user")
+@Api(value="사용자 API")
 public class UserController {
 	@Autowired
 	UserService userService;
@@ -34,7 +42,14 @@ public class UserController {
 	PasswordEncoder passwordEncoder;
 	
 	@PostMapping("/login")
-	public ResponseEntity<?> loginUser(@RequestBody LoginRequest reqUser) {
+	@ApiOperation(value="사용자 로그인", notes ="아이디와 패스워드를 통해 로그인 한다.")
+	@ApiResponses({
+		@ApiResponse(code=200,message="성공",response = LoginResponse.class),
+		@ApiResponse(code=401,message="인증 실패",response = CommonResponse.class),
+		@ApiResponse(code=404,message="사용자 없음",response = CommonResponse.class),
+		@ApiResponse(code=500,message="서버 오류",response = CommonResponse.class)
+	})
+	public ResponseEntity<?> loginUser(@RequestBody @ApiParam(value="로그인 정보",required = true) LoginRequest reqUser) {
 		String email = reqUser.getEmail();
 		String pass = reqUser.getPass();
 		
@@ -43,44 +58,52 @@ public class UserController {
 			
 			if(dbUser==null) {
 				log.info("사용자 정보가 없습니다.");
-				return ResponseEntity.status(404).body("Not Found Email!"); 
+				return ResponseEntity.status(404).body(CommonResponse.of("존재하지 않는 계정입니다.")); 
 			} else if(!passwordEncoder.matches(pass,dbUser.getPass())) {
 				log.info("비밀번호가 일치하지 않습니다.");
-				return ResponseEntity.status(401).body("Incorrect Password!");
+				return ResponseEntity.status(401).body(CommonResponse.of("비밀번호가 일치하지 않습니다."));
 			}
 			
 			String token = TokenProvider.getToken(email);
 			
-			final UserResponse userResponse = new UserResponse(dbUser,token);
-			return ResponseEntity.ok(userResponse);
+			return ResponseEntity.ok(LoginResponse.of("Success", token));
 		} catch(Exception e) {
 			log.info("사용자 로그인 중 오류 발생");
-			return ResponseEntity.status(500).body("Server Error!");
+			return ResponseEntity.status(500).body(CommonResponse.of("서버 오류가 발생했습니다."));
 		}
 	}
 	
 	@PostMapping()
+	@ApiOperation(value = "사용자 생성")
+	@ApiResponses({
+		@ApiResponse(code=200,message="성공",response = CommonResponse.class),
+		@ApiResponse(code=401,message="중복된 이메일",response = CommonResponse.class),
+		@ApiResponse(code=500,message="서버 오류",response = CommonResponse.class)
+	})
 	public ResponseEntity<?> createUser(@RequestBody UserRequest user) {
 		try {
 			userService.createUser(user);
 		} catch(DuplicateRequestException e) {
 			log.info("이미 가입된 이메일입니다.");
-			return ResponseEntity.status(401).body("Exited Email!");
+			return ResponseEntity.status(401).body(CommonResponse.of("이미 가입된 이메일입니다."));
 		} catch(Exception e) {
 			log.info("사용자 생성 중 오류 발생");
-			return ResponseEntity.status(500).body("Server Error!");
+			return ResponseEntity.status(500).body(CommonResponse.of("서버 오류가 발생했습니다."));
 		}
-		return ResponseEntity.ok(user);
+		return ResponseEntity.ok(CommonResponse.of("Success"));
 	}
 	
 	@GetMapping("/me")
+	@ApiOperation(value = "내 정보 가져오기")
+	@ApiResponses({
+		@ApiResponse(code=200,message="성공",response = UserResponse.class),
+		@ApiResponse(code=500,message="서버 오류",response = CommonResponse.class)
+	})
 	public ResponseEntity<?> getUserInfo(Authentication authentication) {
 		CustomUserDetails accountDetailes = (CustomUserDetails) authentication.getDetails();
 		
 		User user = userService.findByUserEmail(accountDetailes.getUseremail());
 		
-		final UserResponse userResponse = new UserResponse(user);
-		
-		return ResponseEntity.ok(userResponse);
+		return ResponseEntity.ok(UserResponse.of("Success", user));
 	}
 }
