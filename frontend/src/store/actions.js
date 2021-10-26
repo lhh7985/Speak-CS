782325a92058146a3dbf9d38dc0ee38045f60b44
xrl0603 <xrl0603@naver.com>
2021-10-26 14:40:28 +0900

    [FE] fix : Axios 요청 수정

--- frontend/src/store/actions.js
+++ frontend/src/store/actions.js
@@ -4,88 +4,88 @@ import axios from "@/lib/axios";
 
 /* ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 유저 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ */
 // 생성하기
-export function requsetUserRegist({ state }, payload) {
+export async function requsetUserRegist({ state }, payload) {
   console.log("requsetUserRegist", state, payload);
   const url = "/user";
   const body = payload;
-  return axios.post(url, body);
+  return await axios.post(url, body);
 }
 // 로그인하기
-export function requestUserLogin({ state }, payload) {
+export async function requestUserLogin({ state }, payload) {
   console.log("requestUserLogin", state, payload);
   const url = "/user/login";
   const body = payload;
-  return axios.post(url, body);
+  return await axios.post(url, body);
 }
 // 유저정보 가져오기
-export function requsetUserInfo({ state }, payload) {
+export async function requsetUserInfo({ state }, payload) {
   console.log("requsetUserInfo", state, payload);
   const url = "/user/me";
   const body = payload;
-  return axios.get(url, body);
+  return await axios.get(url, body);
 }
 // 유저정보 리스트 가져오기
-export function requestUserList({ state }, payload) {
+export async function requestUserList({ state }, payload) {
   console.log("requestUserList", state, payload);
   const url = "/user/all";
   const body = payload;
-  return axios.get(url, body);
+  return await axios.get(url, body);
 }
 
 /* ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 카테고리 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ */
 // 모든 카테고리 정보 가져오기
-export function requsetCategoryCreate({ state }, payload) {
+export async function requsetCategoryCreate({ state }, payload) {
   console.log("requsetCategoryCreate", state, payload);
   const url = "/category";
-  return axios.get(url);
+  return await axios.get(url);
 }
 
 /* ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 문제 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ */
 // 문제만들기
-export function requsetProblemCreate({ state }, payload) {
+export async function requsetProblemCreate({ state }, payload) {
   console.log("requsetProblemCreate", state, payload);
   const url = "/problem/make";
   const body = payload;
-  return axios.post(url, body);
+  return await axios.post(url, body);
 }
 // 카테고리별 문제 리스트 가져오기
-export function requestProblemList({ state }, payload) {
+export async function requestProblemList({ state }, payload) {
   console.log("requestProblemList", state, payload);
   const url = `/problem/${payload.id}}`;
-  return axios.get(url);
+  return await axios.get(url);
 }
 // 문제 정답 맞추기
-export function requestProblemCheckAnswer({ state }, payload) {
+export async function requestProblemCheckAnswer({ state }, payload) {
   console.log("requestProblemCheckAnswer", state, payload);
   const url = "/problem/checkanswer";
   const body = payload;
-  return axios.post(url, body);
+  return await axios.post(url, body);
 }
 // 승인 대기중인 문제 리스트 가져오기
-export function requestProblemWait({ state }, payload) {
+export async function requestProblemWait({ state }, payload) {
   console.log("requestProblemWait", state, payload);
   const url = "/problem/wait";
   const body = payload;
-  return axios.get(url, body);
+  return await axios.get(url, body);
 }
 // 관리자가 문제상태 변경하기
-export function requestProblemEvaluate({ state }, payload) {
+export async function requestProblemEvaluate({ state }, payload) {
   console.log("requestProblemEvaluate", state, payload);
   const url = "/problem/evaluate";
   const body = payload;
-  return axios.post(url, body);
+  return await axios.post(url, body);
 }
 
 /* ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 게시판 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ */
 // 게시판 한개 가져오기
-export function requsetBoardInfo({ state }, payload) {
+export async function requsetBoardInfo({ state }, payload) {
   console.log("requsetBoardInfo", state, payload);
   const url = `/board/${payload.id}`;
-  return axios.get(url);
+  return await axios.get(url);
 }
 // 게시판 리스트 가져오기
-export function requestBoardList({ state }, payload) {
+export async function requestBoardList({ state }, payload) {
   console.log("requestBoardList", state, payload);
   const url = "/board";
-  return axios.get(url);
+  return await axios.get(url);
 }
