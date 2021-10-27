903e49c39b5d50d102570b0d7b019d64f533de93
hohwang <ghghkd52@naver.com>
2021-10-27 13:13:58 +0900

    [BE] refactor: Comment Dto 객체 대댓글 처리

--- backend/src/main/java/com/js/freeproject/domain/comment/dto/CommentResponse.java
+++ backend/src/main/java/com/js/freeproject/domain/comment/dto/CommentResponse.java
@@ -6,6 +6,8 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CommentResponse {
@@ -13,27 +15,31 @@ public class CommentResponse {
     private String content;
     private User user;
     private Long parentId;
+    private LocalDateTime createdAt;
 
     public static final CommentResponse ofComment(final Comment comment){
         final Long id = comment.getId();
         final String content = comment.getContent();;
         final User user = comment.getUser();
+        final LocalDateTime createdAt = comment.getCreatedAt();
         final Comment parent = comment.getParent();
         if(comment.getParent() != null)
-            return new CommentResponse(id, content, user, parent);
-        return new CommentResponse(id, content, user);
+            return new CommentResponse(id, content, user,createdAt, parent);
+        return new CommentResponse(id, content, user,createdAt);
     }
 
-    private CommentResponse(Long id, String content, User user, Comment parent) {
+    private CommentResponse(Long id, String content, User user,LocalDateTime createdAt, Comment parent) {
         this.id = id;
         this.content = content;
         this.user = user;
         this.parentId = parent.getId();
+        this.createdAt = createdAt;
     }
 
-    private CommentResponse(Long id, String content, User user) {
+    private CommentResponse(Long id, String content, User user,LocalDateTime createdAt) {
         this.id = id;
         this.content = content;
         this.user = user;
+        this.createdAt = createdAt;
     }
 }
