8a8a16c762d28ccc0c65f0ff3c83cf4e2fca1910
진해진 <j__0308@naver.com>
2021-10-27 14:45:13 +0900

    [FE] feat : 메뉴 선택시 배경색 추가

--- frontend/src/views/mypage/leftpage.vue
+++ frontend/src/views/mypage/leftpage.vue
@@ -6,10 +6,12 @@
         <span class="user-name text-h4 text-bold">박디두</span>
       </div>
       <div class="mypage-menu text-h6">
-        <div @click="mvProfile">프로필</div>
-        <div @click="mvChart">차트</div>
-        <div @click="mvBoard">나의 게시글</div>
-        <div @click="mvAlarm">알림</div>
+        <div id="menuBtn1" class="menus click-menu" @click="mvProfile">
+          프로필
+        </div>
+        <div id="menuBtn2" class="menus" @click="mvChart">차트</div>
+        <div id="menuBtn3" class="menus" @click="mvBoard">나의 게시글</div>
+        <div id="menuBtn4" class="menus" @click="mvAlarm">알림</div>
       </div>
     </div>
   </div>
@@ -23,16 +25,35 @@ export default {
   setup() {
     const router = useRouter();
     const mvProfile = () => {
+      removeColor();
+      addColor("menuBtn1");
       router.push({ name: "mypage-profile" });
     };
     const mvChart = () => {
+      removeColor();
+      addColor("menuBtn2");
       router.push({ name: "mypage-chart" });
     };
     const mvBoard = () => {
+      removeColor();
+      addColor("menuBtn3");
       router.push({ name: "mypage-board" });
     };
     const mvAlarm = () => {
+      removeColor();
+      addColor("menuBtn4");
       router.push({ name: "mypage-alarm" });
+    };
+
+    const removeColor = () => {
+      const menus = document.getElementsByClassName("menus");
+      for (var i = 0; i < menus.length; i++) {
+        menus[i].classList.remove("click-menu");
+      }
+    };
+    const addColor = (id) => {
+      const btn = document.getElementById(id);
+      btn.classList.add("click-menu");
     };
     return {
       mvProfile,
