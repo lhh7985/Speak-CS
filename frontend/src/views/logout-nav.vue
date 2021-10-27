6476dfda355dfb3d9a0bf527b4114688af3c7b15
진해진 <j__0308@naver.com>
2021-10-27 13:32:37 +0900

    [FE] feat : 로그아웃 시 소개 페이지

--- frontend/src/views/logout-nav.vue
+++ frontend/src/views/logout-nav.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div>
-    <q-btn unelevated flat class="menu-btn">소개</q-btn>
+    <q-btn unelevated flat class="menu-btn" @click="mvLogoutInfo">소개</q-btn>
     <q-btn unelevated flat class="menu-btn" @click="mvRegister">회원가입</q-btn>
     <q-btn unelevated flat class="menu-btn" @click="mvLogin">로그인</q-btn>
   </div>
@@ -15,6 +15,9 @@ export default {
   name: "logout-nav",
   setup() {
     const router = useRouter();
+    const mvLogoutInfo = () => {
+      router.push({ name: "logout-info" });
+    };
     const mvLogin = () => {
       // alert("로그인!");
       // router.push({ name: "login-cover" });
@@ -24,10 +27,10 @@ export default {
       router.push({ name: "register" });
     };
     return {
+      mvLogoutInfo,
       mvLogin,
       mvRegister,
     };
   },
 };
 </script>
-<style lang=""></style>
