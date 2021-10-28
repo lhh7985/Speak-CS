dcc35748a094a6de196c9e9c709a04c3ee1ff8f5
KingBlackCow <sgs1159@naver.com>
2021-10-28 15:13:24 +0900

    [BE] fix : 메인 변경

--- backend/src/main/java/com/js/freeproject/FreeProjectApplication.java
+++ backend/src/main/java/com/js/freeproject/FreeProjectApplication.java
@@ -2,12 +2,15 @@ package com.js.freeproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class FreeProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FreeProjectApplication.class, args);
+        SpringApplication app = new SpringApplication(FreeProjectApplication.class);
+        app.addListeners(new ApplicationPidFileWriter()); // pid 파일을 생성하는 writer 등록
+        app.run(args);
     }
 
 }
