c84dc0e1847e9f93ed726826ef5ba78b96085f65
Seojeonguk <uk7880@naver.com>
2021-10-28 11:15:04 +0900

    [BE] refactor : Use redis and refresh token

--- backend/src/main/java/com/js/freeproject/global/config/SecurityConfig.java
+++ backend/src/main/java/com/js/freeproject/global/config/SecurityConfig.java
@@ -3,12 +3,14 @@ package com.js.freeproject.global.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.js.freeproject.domain.user.appliction.UserService;
 import com.js.freeproject.global.jwt.JwtFilter;
@@ -19,6 +21,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private JwtFilter jwtFilter;
+	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/h2-console/**","/swagger-resources/**","/swagger-ui.html","/swagger/**");
@@ -28,7 +33,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.csrf().disable()
-			.addFilter(new JwtFilter(authenticationManager(),userService))
 			.authorizeRequests()
 			.antMatchers("/user/me").authenticated()
 			.antMatchers("/user/**").permitAll()
@@ -38,7 +42,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 			.httpBasic()
 			.and()
-			.cors();
+			.cors()
+			.and()
+			.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 	
 	@Bean
