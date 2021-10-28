9dd4009e46bfcba0ac41c3cf14e2d20cbd5b47fc
Seojeonguk <uk7880@naver.com>
2021-10-28 11:10:47 +0900

    [BE] refactor : fix redistemplate

--- backend/src/main/java/com/js/freeproject/global/util/RedisUtil.java
+++ backend/src/main/java/com/js/freeproject/global/util/RedisUtil.java
@@ -1,34 +1,40 @@
-package com.js.freeproject.global.redis;
+package com.js.freeproject.global.util;
 
 import java.time.Duration;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.js.freeproject.domain.user.appliction.UserService;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class RedisUtil {
-	@Autowired
-	private StringRedisTemplate stringRedisTemplate;
+	private final RedisTemplate<String, String> redisTemplate;
+
 	
 	public String getData(String key){
-        ValueOperations<String,String> valueOperations = stringRedisTemplate.opsForValue();
+        ValueOperations<String,String> valueOperations = redisTemplate.opsForValue();
         return valueOperations.get(key);
     }
 
     public void setData(String key, String value){
-        ValueOperations<String,String> valueOperations = stringRedisTemplate.opsForValue();
+        ValueOperations<String,String> valueOperations = redisTemplate.opsForValue();
         valueOperations.set(key,value);
     }
 
     public void setDataExpire(String key,String value,long duration){
-        ValueOperations<String,String> valueOperations = stringRedisTemplate.opsForValue();
+        ValueOperations<String,String> valueOperations = redisTemplate.opsForValue();
         Duration expireDuration = Duration.ofSeconds(duration);
         valueOperations.set(key,value,expireDuration);
     }
 
     public void deleteData(String key){
-        stringRedisTemplate.delete(key);
+    	redisTemplate.delete(key);
     }
 }
