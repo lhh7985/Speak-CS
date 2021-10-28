f52018dd1aee6c85320dd7104b0b44ed70c7ce7d
Seojeonguk <uk7880@naver.com>
2021-10-28 12:59:08 +0900

    [BE} feat : Add registration bean

--- backend/src/main/java/com/js/freeproject/global/jwt/JwtFilter.java
+++ backend/src/main/java/com/js/freeproject/global/jwt/JwtFilter.java
@@ -7,6 +7,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -125,6 +127,13 @@ public class JwtFilter extends OncePerRequestFilter {
 
 	private final UserService userService;
 	private final RedisUtil redisUtil;
+	
+	@Bean
+	public FilterRegistrationBean JwtFilterRegistration(JwtFilter filter) {
+		FilterRegistrationBean registration = new FilterRegistrationBean(filter);
+		registration.setEnabled(false);
+		return registration;
+	}
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
