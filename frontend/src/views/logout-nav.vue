1aa1349297c28c34d57fda8f2afca604376188fe
진해진 <j__0308@naver.com>
2021-10-28 10:27:49 +0900

    [FE] feat : 로그아웃 커버 메뉴 선택시 색 변경

--- frontend/src/views/logout-nav.vue
+++ frontend/src/views/logout-nav.vue
@@ -6,17 +6,37 @@
       <q-btn
         unelevated
         flat
-        class="menu-btn menu-btn-cover"
+        id="menuBtn1"
+        class="menu-btn menu-btn-cover menus click-menu"
         @click="mvLogoutCover"
         >커버</q-btn
       >
     </div>
     <div>
-      <q-btn unelevated flat class="menu-btn" @click="mvLogoutInfo">소개</q-btn>
-      <q-btn unelevated flat class="menu-btn" @click="mvRegister"
+      <q-btn
+        unelevated
+        flat
+        id="menuBtn2"
+        class="menu-btn menus"
+        @click="mvLogoutInfo"
+        >소개</q-btn
+      >
+      <q-btn
+        unelevated
+        flat
+        id="menuBtn3"
+        class="menu-btn menus"
+        @click="mvRegister"
         >회원가입</q-btn
       >
-      <q-btn unelevated flat class="menu-btn" @click="mvLogin">로그인</q-btn>
+      <q-btn
+        unelevated
+        flat
+        id="menuBtn4"
+        class="menu-btn menus"
+        @click="mvLogin"
+        >로그인</q-btn
+      >
     </div>
   </div>
 </template>
@@ -29,24 +49,41 @@ export default {
   setup() {
     const router = useRouter();
     const mvLogoutCover = () => {
+      removeColor();
+      addColor("menuBtn1");
       router.push({ name: "logout-cover" });
     };
     const mvLogoutInfo = () => {
+      removeColor();
+      addColor("menuBtn2");
       router.push({ name: "logout-info" });
     };
-    const mvLogin = () => {
-      // alert("로그인!");
-      // router.push({ name: "login-cover" });
-      router.push({ name: "login" });
-    };
     const mvRegister = () => {
+      removeColor();
+      addColor("menuBtn3");
       router.push({ name: "register" });
     };
+    const mvLogin = () => {
+      removeColor();
+      addColor("menuBtn4");
+      router.push({ name: "login" });
+    };
+    const removeColor = () => {
+      const menus = document.getElementsByClassName("menus");
+      for (var i = 0; i < menus.length; i++) {
+        menus[i].classList.remove("click-menu");
+      }
+    };
+    const addColor = (id) => {
+      const btn = document.getElementById(id);
+      btn.classList.add("click-menu");
+    };
+
     return {
       mvLogoutCover,
       mvLogoutInfo,
-      mvLogin,
       mvRegister,
+      mvLogin,
     };
   },
 };
