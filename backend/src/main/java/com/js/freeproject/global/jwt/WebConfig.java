2ea07b8dfcb1411ddea6f33cb7552701764b4604
Seojeonguk <uk7880@naver.com>
2021-10-28 11:11:47 +0900

    [BE} feat : Add webconfig

---
+++ backend/src/main/java/com/js/freeproject/global/jwt/WebConfig.java
@@ -0,0 +1,14 @@
+package com.js.freeproject.global.jwt;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+		.allowedMethods("GET","PUT","POST","DELETE");
+	}
+}
