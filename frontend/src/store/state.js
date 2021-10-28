484ef638ffa6782425fe3c42f4c680f6bf2549d2
xrl0603 <xrl0603@naver.com>
2021-10-28 12:00:29 +0900

    [FE] feat : 사용자정보 저장 변수 생성

--- frontend/src/store/state.js
+++ frontend/src/store/state.js
@@ -1,5 +1,7 @@
 export default {
-  item: [],
+  /* ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 유저 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ */
+  user: {}, // 유저정보
+  /* ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 문제 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ */
   categories: [], //모든 카테고리
   selectedProblems: [], //카테고리에서 선택된 문제들
   problemResults: [], //정답 결과
