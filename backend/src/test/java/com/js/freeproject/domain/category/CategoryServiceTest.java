3c3aa65e7c42d06510649ec0fe6b7618c0467723
KingBlackCow <sgs1159@naver.com>
2021-10-28 16:29:47 +0900

    [BE] fix : catagoryTEst주석처리

--- backend/src/test/java/com/js/freeproject/domain/category/CategoryServiceTest.java
+++ backend/src/test/java/com/js/freeproject/domain/category/CategoryServiceTest.java
@@ -1,50 +1,50 @@
-package com.js.freeproject.domain.category;
-
-import com.js.freeproject.domain.category.application.CategoryService;
-import com.js.freeproject.domain.category.domain.Category;
-import com.js.freeproject.domain.category.domain.CategoryRepository;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.EntityManager;
-import javax.swing.text.html.parser.Entity;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
-@Transactional
-class CategoryServiceTest {
-    @Autowired
-    EntityManager em;
-
-    @Autowired
-    CategoryService categoryService;
-
-    @Autowired
-    CategoryRepository categoryRepository;
-
-    @Test
-    public void saveCategoryTest(){
-        Category category = new Category("운영체제","컴퓨터구조를 배워봐요");
-        em.persist(category);
-
-        Category category2 = new Category("네트워크","이게 네트워크지");
-        em.persist(category2);
-
-        em.flush();
-        em.clear();
-
-        List<Category> categories = categoryService.findCategorys();
-
-        assertThat(categories.size()).isEqualTo(2);
-        for (Category category1: categories) {
-            System.out.println(category1.getName());
-        }
-    }
-}
+//package com.js.freeproject.domain.category;
+//
+//import com.js.freeproject.domain.category.application.CategoryService;
+//import com.js.freeproject.domain.category.domain.Category;
+//import com.js.freeproject.domain.category.domain.CategoryRepository;
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import javax.persistence.EntityManager;
+//import javax.swing.text.html.parser.Entity;
+//
+//import java.util.List;
+//
+//import static org.assertj.core.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest
+//@Transactional
+//class CategoryServiceTest {
+//    @Autowired
+//    EntityManager em;
+//
+//    @Autowired
+//    CategoryService categoryService;
+//
+//    @Autowired
+//    CategoryRepository categoryRepository;
+//
+//    @Test
+//    public void saveCategoryTest(){
+//        Category category = new Category("운영체제","컴퓨터구조를 배워봐요");
+//        em.persist(category);
+//
+//        Category category2 = new Category("네트워크","이게 네트워크지");
+//        em.persist(category2);
+//
+//        em.flush();
+//        em.clear();
+//
+//        List<Category> categories = categoryService.findCategorys();
+//
+//        assertThat(categories.size()).isEqualTo(2);
+//        for (Category category1: categories) {
+//            System.out.println(category1.getName());
+//        }
+//    }
+//}
