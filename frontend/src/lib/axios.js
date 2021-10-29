d5b3c474f0532fc146a9047b8aeabe9f9db71396
xrl0603 <xrl0603@naver.com>
2021-10-29 11:37:10 +0900

    [FE] fix : AXios BaseUrl 수정

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const instance = axios.create({
   // baseURL: "/",
   // baseURL: "/api/v1",
-  baseURL: "http://localhost:8080",
-  // baseURL: "http://j5c201.p.ssafy.io/api/v1",
+  // baseURL: "http://localhost:8080",
+  baseURL: "http://k5c101.p.ssafy.io:3306",
   headers: {
     "Content-type": "application/json",
   },
