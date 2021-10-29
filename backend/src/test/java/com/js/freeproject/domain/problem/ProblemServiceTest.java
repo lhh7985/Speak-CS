2d19101476cb13460ead11cb41e90c703c450812
KingBlackCow <sgs1159@naver.com>
2021-10-29 14:15:48 +0900

    [BE] fix : 테스트코드 주석처리

--- backend/src/test/java/com/js/freeproject/domain/problem/ProblemServiceTest.java
+++ backend/src/test/java/com/js/freeproject/domain/problem/ProblemServiceTest.java
@@ -10,6 +10,7 @@
 //import com.js.freeproject.domain.problempicture.domain.ProblemPicture;
 //import com.js.freeproject.domain.problempicture.domain.ProblemPictureRepository;
 //import com.js.freeproject.domain.user.domain.User;
+//import com.js.freeproject.domain.user.domain.UserRepository;
 //import org.junit.jupiter.api.BeforeEach;
 //import org.junit.jupiter.api.Test;
 //import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,7 @@
 //
 //import javax.persistence.EntityManager;
 //
+//import java.util.ArrayList;
 //import java.util.List;
 //import java.util.Optional;
 //
@@ -45,6 +47,9 @@
 //    @Autowired
 //    CategoryRepository categoryRepository;
 //
+//    @Autowired
+//    UserRepository userRepository;
+//
 ////    @BeforeEach
 ////    @Rollback(false)
 ////    public void before() {
@@ -61,10 +66,14 @@
 //    @Test
 //    @Rollback(false)
 //    void saveProblem() {
-//        Category category = new Category("스프링", "스프링이 무엇일까");
-//        em.persist(category);
-//        User user = new User("sgs1159@naver.com", "순조에용", "이조순", "1234", "팀원", "유저디폴트이미지");
-//        em.persist(user);
+//        //Category category = new Category("스프링", "스프링이 무엇일까");
+//        //em.persist(category);
+//        Category category = categoryRepository.findById(3L).get();
+//        //User user = new User("sgs1159@naver.com", "순조에용", "이조순", "1234", "팀원", "유저디폴트이미지");
+//        User user = userRepository.findByNickName("조순몬")
+//                .orElseThrow(() -> new IllegalArgumentException("못찾음"));
+//
+////        em.persist(user);
 //        ProblemPicture problemPicture = new ProblemPicture("디폴트이미지 url");
 //
 //        Answer answer1 = Answer.createAnswer("mvc");
@@ -72,9 +81,14 @@
 //        Answer answer3 = Answer.createAnswer("스프링부트");
 //        Answer answer4 = Answer.createAnswer("클라우드");
 //        Answer answer5 = Answer.createAnswer("배치");
-//
-//        Problem problem = Problem.createProblem("스프링이 무엇인가?",
-//                category, user, problemPicture, answer1, answer2, answer3, answer4, answer5);
+//        List<Answer> list = new ArrayList<>();
+//        list.add(answer1);
+//        list.add(answer2);
+//        list.add(answer3);
+//        list.add(answer4);
+//        list.add(answer5);
+//        Problem problem = Problem.createProblem("스프링과 스프링부트의 차이는?",
+//                category, user, new ArrayList<>(), list);
 //        System.out.println(problem);
 //        em.persist(problem);
 //        em.flush();
@@ -96,108 +110,97 @@
 //
 //        }
 //    }
-//
 //    @Test
-//    @Rollback(false)
-//    void findProblemsByCategoryId() {
-//        //Category category = new Category("스프링", "스프링이 무엇일까");
-//        //em.persist(category);
-//        Category category = categoryRepository.findById(3L).get();
-//        User user = new User("sgs1159@naver.com", "순조에용", "이조순", "1234", "팀원", "유저디폴트이미지");
-//        em.persist(user);
-//
-//        ProblemPicture problemPicture = new ProblemPicture("디폴트이미지 url");
-//        Answer answer1 = Answer.createAnswer("mvc");
-//        Answer answer2 = Answer.createAnswer("모델");
-//        Answer answer3 = Answer.createAnswer("스프링부트");
-//        Answer answer4 = Answer.createAnswer("클라우드");
-//        Answer answer5 = Answer.createAnswer("배치");
-//
-//        Problem problem = Problem.createProblem("스프링이 무엇인가?",
-//                category, user, problemPicture, answer1, answer2, answer3, answer4, answer5);
-//        //System.out.println(problem);
-//        em.persist(problem);
-//
-//
-//        Problem problem2 = Problem.createProblem("REST API란?",
-//                category, user, problemPicture, answer3, answer4, answer5);
-//        em.persist(problem2);
-//
-//        Problem problem3 = Problem.createProblem("이게스프링이지",
-//                category, user, problemPicture, answer2);
-//        em.persist(problem3);
-//        PageRequest pageRequest = PageRequest.of(0, 3);
-//
-//
-//        em.flush();
-//        em.clear();
-//
-//        List<Problem> all = problemRepository.findByCategory("스프링", pageRequest);
-//        for (Problem p : all) {
-//            System.out.println(p);
-//        }
-//        assertThat(all.size()).isEqualTo(3);
-//
-//    }
-//
-//    @Test
-//    @Rollback(false)
-//    void findByStatus() {
-//        Category category = new Category("스프링", "스프링이 무엇일까");
-//        em.persist(category);
-//
-//        User user = new User("sgs1159@naver.com", "순조에용", "이조순", "1234", "팀원", "유저디폴트이미지");
-//        em.persist(user);
-//
-//        ProblemPicture problemPicture = new ProblemPicture("디폴트이미지 url");
-//        Answer answer1 = Answer.createAnswer("mvc");
-//        Answer answer2 = Answer.createAnswer("모델");
-//        Answer answer3 = Answer.createAnswer("스프링부트");
-//        Answer answer4 = Answer.createAnswer("클라우드");
-//        Answer answer5 = Answer.createAnswer("배치");
-//
-//        Problem problem = Problem.createProblem("스프링이 무엇인가?",
-//                category, user, problemPicture, answer1, answer2, answer3, answer4, answer5);
-//        em.persist(problem);
-//
-//        Problem problem2 = Problem.createProblem("REST API란?",
-//                category, user, problemPicture, answer3, answer4, answer5);
-//        em.persist(problem2);
-//
-//        em.flush();
-//        em.clear();
-//
-//        List<Problem> waitList = problemRepository.findByStatus(ProblemStatus.wait);
-//        for (Problem p: waitList) {
-//            System.out.println(p);
+//    void findAnswer(){
+//        Problem problem = problemRepository.findById(1L).get();
+//        for (Answer answer:problem.getAnswers()) {
+//            System.out.println(answer.getWord());
 //        }
 //    }
 //
+////    @Test
+////    @Rollback(false)
+////    void findProblemsByCategoryId() {
+////        //Category category = new Category("스프링", "스프링이 무엇일까");
+////        //em.persist(category);
+////        Category category = categoryRepository.findById(3L).get();
+////        User user = new User("sgs1159@naver.com", "순조에용", "이조순", "1234", "팀원", "유저디폴트이미지");
+////        em.persist(user);
+////
+////        ProblemPicture problemPicture = new ProblemPicture("디폴트이미지 url");
+////        Answer answer1 = Answer.createAnswer("mvc");
+////        Answer answer2 = Answer.createAnswer("모델");
+////        Answer answer3 = Answer.createAnswer("스프링부트");
+////        Answer answer4 = Answer.createAnswer("클라우드");
+////        Answer answer5 = Answer.createAnswer("배치");
+////
+////        Problem problem = Problem.createProblem("스프링이 무엇인가?",
+////                category, user, problemPicture, answer1, answer2, answer3, answer4, answer5);
+////        //System.out.println(problem);
+////        em.persist(problem);
+////
+////
+////        Problem problem2 = Problem.createProblem("REST API란?",
+////                category, user, problemPicture, answer3, answer4, answer5);
+////        em.persist(problem2);
+////
+////        Problem problem3 = Problem.createProblem("이게스프링이지",
+////                category, user, problemPicture, answer2);
+////        em.persist(problem3);
+////        PageRequest pageRequest = PageRequest.of(0, 3);
+////
+////
+////        em.flush();
+////        em.clear();
+////
+////        List<Problem> all = problemRepository.findByCategory("스프링", pageRequest);
+////        for (Problem p : all) {
+////            System.out.println(p);
+////        }
+////        assertThat(all.size()).isEqualTo(3);
+////
+////    }
+////
+////    @Test
+////    @Rollback(false)
+////    void findByStatus() {
+////        Category category = new Category("스프링", "스프링이 무엇일까");
+////        em.persist(category);
+////
+////        User user = new User("sgs1159@naver.com", "순조에용", "이조순", "1234", "팀원", "유저디폴트이미지");
+////        em.persist(user);
+////
+////        ProblemPicture problemPicture = new ProblemPicture("디폴트이미지 url");
+////        Answer answer1 = Answer.createAnswer("mvc");
+////        Answer answer2 = Answer.createAnswer("모델");
+////        Answer answer3 = Answer.createAnswer("스프링부트");
+////        Answer answer4 = Answer.createAnswer("클라우드");
+////        Answer answer5 = Answer.createAnswer("배치");
+////
+////        Problem problem = Problem.createProblem("스프링이 무엇인가?",
+////                category, user, problemPicture, answer1, answer2, answer3, answer4, answer5);
+////        em.persist(problem);
+////
+////        Problem problem2 = Problem.createProblem("REST API란?",
+////                category, user, problemPicture, answer3, answer4, answer5);
+////        em.persist(problem2);
+////
+////        em.flush();
+////        em.clear();
+////
+////        List<Problem> waitList = problemRepository.findByStatus(ProblemStatus.wait);
+////        for (Problem p: waitList) {
+////            System.out.println(p);
+////        }
+////    }
+////
 //    @Test
+//    @Rollback(false)
 //    void updateProblem(){
-//        Category category = new Category("스프링", "스프링 입니다.");
-//        em.persist(category);
-//
-//        User user = new User("sgs1159@naver.com", "순조에용", "이조순", "1234", "팀원", "유저디폴트이미지");
-//        em.persist(user);
-//
-//        ProblemPicture problemPicture = new ProblemPicture("디폴트이미지 url");
-//        Answer answer1 = Answer.createAnswer("mvc");
-//        Answer answer2 = Answer.createAnswer("모델");
-//
-//
-//        Problem problem = Problem.createProblem("스프링이 무엇인가?",
-//                category, user, problemPicture, answer1, answer2);
-//        em.persist(problem);
-//        em.flush();
-//        em.clear();
-//
-//        //System.out.println("아이디는?"+problem.getId());
-//        Optional<Problem> one = problemRepository.findById(problem.getId());
-//        if(one.isPresent()){
-//            one.get().setStatus(ProblemStatus.accept);
+//        List<Problem> one = problemRepository.findAll();
+//        for(Problem p: one){
+//            p.setStatus(ProblemStatus.accept);
 //        }
-//        em.persist(one.get());
 //        em.flush();
 //        em.clear();
 //
