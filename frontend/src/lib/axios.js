93ca9acdf5d7621119ae9555038b512ebec3bfe9
xrl0603 <xrl0603@naver.com>
2021-10-29 11:45:53 +0900

    [FE] hotfix : axios baseURL 수정

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ const instance = axios.create({
   // baseURL: "/",
   // baseURL: "/api/v1",
   // baseURL: "http://localhost:8080",
-  baseURL: "http://k5c101.p.ssafy.io",
+  baseURL: "http://k5c101.p.ssafy.io/api",
   headers: {
     "Content-type": "application/json",
   },
