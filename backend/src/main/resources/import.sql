a6c3f310f681a4263c1d6434502877d20f525875
KingBlackCow <sgs1159@naver.com>
2021-10-29 14:57:58 +0900

    [BE] delete : import sql코드 주석처리

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,2 +1,2 @@
-insert into user(email, nick_name, name,pass,role) values('admin','관리자','관리자','$2a$10$rvNYah11hiVaf53P1nwxKOHqPgxJOkfVMIyqGAnHmGKY8ZtOqZ5h2','ROLE_ADMIN');
-insert into user(email, nick_name, name,pass) values('various@naver.com','홍길동의 후예','홍길동','$2a$10$r0ealayqf/Zq1xOTmQZccuxz54UN7kvk7rP0WKdQCSTR4KS6vXWgi');
+--insert into user(email, nick_name, name,pass,role) values('admin','관리자','관리자','$2a$10$rvNYah11hiVaf53P1nwxKOHqPgxJOkfVMIyqGAnHmGKY8ZtOqZ5h2','ROLE_ADMIN');
+--insert into user(email, nick_name, name,pass) values('various@naver.com','홍길동의 후예','홍길동','$2a$10$r0ealayqf/Zq1xOTmQZccuxz54UN7kvk7rP0WKdQCSTR4KS6vXWgi');
