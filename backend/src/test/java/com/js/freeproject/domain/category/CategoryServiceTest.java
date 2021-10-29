1d15be595c0c7e3f7e5b0a5c8db60abd113d340d
KingBlackCow <sgs1159@naver.com>
2021-10-29 14:58:27 +0900

    [BE] feat : 테스트 코드 작성

--- backend/src/test/java/com/js/freeproject/domain/category/CategoryServiceTest.java
+++ backend/src/test/java/com/js/freeproject/domain/category/CategoryServiceTest.java
@@ -1,68 +1,73 @@
-//package com.js.freeproject.domain.category;
-//
-//import com.js.freeproject.domain.category.application.CategoryService;
-//import com.js.freeproject.domain.category.domain.Category;
-//import com.js.freeproject.domain.category.domain.CategoryRepository;
-//import com.js.freeproject.domain.user.domain.User;
-//import com.js.freeproject.domain.user.domain.UserRepository;
-//import org.assertj.core.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.annotation.Rollback;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import javax.persistence.EntityManager;
-//import javax.swing.text.html.parser.Entity;
-//
-//import java.util.List;
-//
-//import static org.assertj.core.api.Assertions.*;
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//@SpringBootTest
-//@Transactional
-//class CategoryServiceTest {
-//    @Autowired
-//    EntityManager em;
-//
-//    @Autowired
-//    CategoryService categoryService;
-//
-//    @Autowired
-//    CategoryRepository categoryRepository;
-//
-//    @Autowired
-//    UserRepository userRepository;
-//
-//    @Test
-//    @Rollback(false)
-//    public void saveCategoryTest(){
-//        User user = User.builder()
-//                .email("sgs1159@naver.com")
-//                .nickName("조순몬")
-//                .name("이조순")
-//                .pass("abcd1234")
-//                .image("abd")
-//                .build();
-//        userRepository.save(user);
-//        em.persist(user);
-//        em.flush();
-//        em.clear();
-//        Category category = new Category("운영체제","컴퓨터구조를 배워봐요");
-//        em.persist(category);
-//
-//        Category category2 = new Category("네트워크","이게 네트워크지");
-//        em.persist(category2);
-//
-//        em.flush();
-//        em.clear();
-//
-//        List<Category> categories = categoryService.findCategorys();
-//
-//        assertThat(categories.size()).isEqualTo(2);
-//        for (Category category1: categories) {
-//            System.out.println(category1.getName());
-//        }
-//    }
-//}
+package com.js.freeproject.domain.category;
+
+import com.js.freeproject.domain.category.application.CategoryService;
+import com.js.freeproject.domain.category.domain.Category;
+import com.js.freeproject.domain.category.domain.CategoryRepository;
+import com.js.freeproject.domain.user.domain.User;
+import com.js.freeproject.domain.user.domain.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.swing.text.html.parser.Entity;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class CategoryServiceTest {
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    CategoryService categoryService;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    @Rollback(false)
+    public void saveCategoryTest(){
+        User user = User.builder()
+                .email("sgs1159@naver.com")
+                .nickName("조순몬")
+                .name("이조순")
+                .pass("abcd1234")
+                .image("abd")
+                .build();
+        userRepository.save(user);
+        em.persist(user);
+        em.flush();
+        em.clear();
+        Category category = new Category("운영체제","컴퓨터의 기본! CS는 운영체제부터!");
+        em.persist(category);
+
+        Category category2 = new Category("네트워크","네트워크 모르면 취업못하는거 알지?");
+        em.persist(category2);
+
+        Category category3 = new Category("스프링","백엔드는 스프링이지ㅋ");
+        em.persist(category3);
+
+        Category category4 = new Category("VUE","리액트하는 사람 다죽었으면");
+        em.persist(category4);
+
+        em.flush();
+        em.clear();
+
+        List<Category> categories = categoryService.findCategorys();
+
+        for (Category category1: categories) {
+            System.out.println(category1.getName());
+        }
+    }
+}
--- backend/src/test/java/com/js/freeproject/domain/problem/ProblemServiceTest.java
+++ backend/src/test/java/com/js/freeproject/domain/problem/ProblemServiceTest.java
@@ -1,209 +1,205 @@
-//package com.js.freeproject.domain.problem;
+package com.js.freeproject.domain.problem;
+
+import com.js.freeproject.domain.answer.domain.Answer;
+import com.js.freeproject.domain.category.domain.Category;
+import com.js.freeproject.domain.category.domain.CategoryRepository;
+import com.js.freeproject.domain.problem.application.ProblemService;
+import com.js.freeproject.domain.problem.domain.Problem;
+import com.js.freeproject.domain.problem.domain.ProblemRepository;
+import com.js.freeproject.domain.problem.domain.ProblemStatus;
+import com.js.freeproject.domain.problempicture.domain.ProblemPicture;
+import com.js.freeproject.domain.problempicture.domain.ProblemPictureRepository;
+import com.js.freeproject.domain.user.domain.User;
+import com.js.freeproject.domain.user.domain.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class ProblemServiceTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    ProblemService problemService;
+
+    @Autowired
+    ProblemRepository problemRepository;
+
+    @Autowired
+    ProblemPictureRepository problemPictureRepository;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+//    @BeforeEach
+//    @Rollback(false)
+//    public void before() {
+//        Category category = new Category("운영체제", "운영체제입니다.");
+//        em.persist(category);
 //
-//import com.js.freeproject.domain.answer.domain.Answer;
-//import com.js.freeproject.domain.category.domain.Category;
-//import com.js.freeproject.domain.category.domain.CategoryRepository;
-//import com.js.freeproject.domain.problem.application.ProblemService;
-//import com.js.freeproject.domain.problem.domain.Problem;
-//import com.js.freeproject.domain.problem.domain.ProblemRepository;
-//import com.js.freeproject.domain.problem.domain.ProblemStatus;
-//import com.js.freeproject.domain.problempicture.domain.ProblemPicture;
-//import com.js.freeproject.domain.problempicture.domain.ProblemPictureRepository;
-//import com.js.freeproject.domain.user.domain.User;
-//import com.js.freeproject.domain.user.domain.UserRepository;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.data.domain.PageRequest;
-//import org.springframework.test.annotation.Rollback;
-//import org.springframework.transaction.annotation.Transactional;
+//        Category category2 = new Category("네트워크", "네트워크입니다.");
+//        em.persist(category2);
 //
-//import javax.persistence.EntityManager;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Optional;
-//
-//import static org.assertj.core.api.Assertions.*;
-//
-//@SpringBootTest
-//@Transactional
-//@Rollback(false)
-//class ProblemServiceTest {
-//
-//    @Autowired
-//    EntityManager em;
-//
-//    @Autowired
-//    ProblemService problemService;
-//
-//    @Autowired
-//    ProblemRepository problemRepository;
-//
-//    @Autowired
-//    ProblemPictureRepository problemPictureRepository;
-//
-//    @Autowired
-//    CategoryRepository categoryRepository;
-//
-//    @Autowired
-//    UserRepository userRepository;
-//
-////    @BeforeEach
-////    @Rollback(false)
-////    public void before() {
-////        Category category = new Category("운영체제", "운영체제입니다.");
-////        em.persist(category);
-////
-////        Category category2 = new Category("네트워크", "네트워크입니다.");
-////        em.persist(category2);
-////
-////        em.flush();
-////        em.clear();
-////    }
-//
+//        em.flush();
+//        em.clear();
+//    }
+
+    @Test
+    @Rollback(false)
+    void saveProblem() {
+        //Category category = new Category("스프링", "스프링이 무엇일까");
+        //em.persist(category);
+        Category category = categoryRepository.findById(3L).get();
+        //User user = new User("sgs1159@naver.com", "순조에용", "이조순", "1234", "팀원", "유저디폴트이미지");
+        User user = userRepository.findByNickName("조순몬")
+                .orElseThrow(() -> new IllegalArgumentException("못찾음"));
+
+//        em.persist(user);
+        ProblemPicture problemPicture = new ProblemPicture("디폴트이미지 url");
+
+//        Answer answer1 = Answer.createAnswer("객체");
+        Answer answer2 = Answer.createAnswer("데이터베이스");
+        Answer answer3 = Answer.createAnswer("교환");
+        List<Answer> list = new ArrayList<>();
+        //list.add(answer1);
+        list.add(answer2);
+        list.add(answer3);
+
+        Problem problem = Problem.createProblem("DTO를 설명해주세요.",
+                category, user, new ArrayList<>(), list);
+        System.out.println(problem);
+        em.persist(problem);
+        em.flush();
+        em.clear();
+
+
+        List<Problem> all = problemRepository.findAll();
+        for (Problem p : all) {
+            System.out.println(p);
+        }
+    }
+
+    @Test
+    void findProblems() {
+        Optional<Problem> problems = problemRepository.findById(1L);
+        for (Answer a: problems.get().getAnswers()) {
+            System.out.println(a.getWord());
+
+        }
+    }
+    @Test
+    void findAnswer(){
+        Problem problem = problemRepository.findById(1L).get();
+        for (Answer answer:problem.getAnswers()) {
+            System.out.println(answer.getWord());
+        }
+    }
+
 //    @Test
 //    @Rollback(false)
-//    void saveProblem() {
+//    void findProblemsByCategoryId() {
 //        //Category category = new Category("스프링", "스프링이 무엇일까");
 //        //em.persist(category);
 //        Category category = categoryRepository.findById(3L).get();
-//        //User user = new User("sgs1159@naver.com", "순조에용", "이조순", "1234", "팀원", "유저디폴트이미지");
-//        User user = userRepository.findByNickName("조순몬")
-//                .orElseThrow(() -> new IllegalArgumentException("못찾음"));
+//        User user = new User("sgs1159@naver.com", "순조에용", "이조순", "1234", "팀원", "유저디폴트이미지");
+//        em.persist(user);
 //
-////        em.persist(user);
 //        ProblemPicture problemPicture = new ProblemPicture("디폴트이미지 url");
-//
 //        Answer answer1 = Answer.createAnswer("mvc");
 //        Answer answer2 = Answer.createAnswer("모델");
 //        Answer answer3 = Answer.createAnswer("스프링부트");
 //        Answer answer4 = Answer.createAnswer("클라우드");
 //        Answer answer5 = Answer.createAnswer("배치");
-//        List<Answer> list = new ArrayList<>();
-//        list.add(answer1);
-//        list.add(answer2);
-//        list.add(answer3);
-//        list.add(answer4);
-//        list.add(answer5);
-//        Problem problem = Problem.createProblem("스프링과 스프링부트의 차이는?",
-//                category, user, new ArrayList<>(), list);
-//        System.out.println(problem);
+//
+//        Problem problem = Problem.createProblem("스프링이 무엇인가?",
+//                category, user, problemPicture, answer1, answer2, answer3, answer4, answer5);
+//        //System.out.println(problem);
 //        em.persist(problem);
+//
+//
+//        Problem problem2 = Problem.createProblem("REST API란?",
+//                category, user, problemPicture, answer3, answer4, answer5);
+//        em.persist(problem2);
+//
+//        Problem problem3 = Problem.createProblem("이게스프링이지",
+//                category, user, problemPicture, answer2);
+//        em.persist(problem3);
+//        PageRequest pageRequest = PageRequest.of(0, 3);
+//
+//
 //        em.flush();
 //        em.clear();
 //
-//
-//        List<Problem> all = problemRepository.findAll();
+//        List<Problem> all = problemRepository.findByCategory("스프링", pageRequest);
 //        for (Problem p : all) {
 //            System.out.println(p);
 //        }
-//        //assertThat(size).isEqualTo(1);
+//        assertThat(all.size()).isEqualTo(3);
+//
 //    }
 //
-//    @Test
-//    void findProblems() {
-//        Optional<Problem> problems = problemRepository.findById(1L);
-//        for (Answer a: problems.get().getAnswers()) {
-//            System.out.println(a.getWord());
-//
-//        }
-//    }
-//    @Test
-//    void findAnswer(){
-//        Problem problem = problemRepository.findById(1L).get();
-//        for (Answer answer:problem.getAnswers()) {
-//            System.out.println(answer.getWord());
-//        }
-//    }
-//
-////    @Test
-////    @Rollback(false)
-////    void findProblemsByCategoryId() {
-////        //Category category = new Category("스프링", "스프링이 무엇일까");
-////        //em.persist(category);
-////        Category category = categoryRepository.findById(3L).get();
-////        User user = new User("sgs1159@naver.com", "순조에용", "이조순", "1234", "팀원", "유저디폴트이미지");
-////        em.persist(user);
-////
-////        ProblemPicture problemPicture = new ProblemPicture("디폴트이미지 url");
-////        Answer answer1 = Answer.createAnswer("mvc");
-////        Answer answer2 = Answer.createAnswer("모델");
-////        Answer answer3 = Answer.createAnswer("스프링부트");
-////        Answer answer4 = Answer.createAnswer("클라우드");
-////        Answer answer5 = Answer.createAnswer("배치");
-////
-////        Problem problem = Problem.createProblem("스프링이 무엇인가?",
-////                category, user, problemPicture, answer1, answer2, answer3, answer4, answer5);
-////        //System.out.println(problem);
-////        em.persist(problem);
-////
-////
-////        Problem problem2 = Problem.createProblem("REST API란?",
-////                category, user, problemPicture, answer3, answer4, answer5);
-////        em.persist(problem2);
-////
-////        Problem problem3 = Problem.createProblem("이게스프링이지",
-////                category, user, problemPicture, answer2);
-////        em.persist(problem3);
-////        PageRequest pageRequest = PageRequest.of(0, 3);
-////
-////
-////        em.flush();
-////        em.clear();
-////
-////        List<Problem> all = problemRepository.findByCategory("스프링", pageRequest);
-////        for (Problem p : all) {
-////            System.out.println(p);
-////        }
-////        assertThat(all.size()).isEqualTo(3);
-////
-////    }
-////
-////    @Test
-////    @Rollback(false)
-////    void findByStatus() {
-////        Category category = new Category("스프링", "스프링이 무엇일까");
-////        em.persist(category);
-////
-////        User user = new User("sgs1159@naver.com", "순조에용", "이조순", "1234", "팀원", "유저디폴트이미지");
-////        em.persist(user);
-////
-////        ProblemPicture problemPicture = new ProblemPicture("디폴트이미지 url");
-////        Answer answer1 = Answer.createAnswer("mvc");
-////        Answer answer2 = Answer.createAnswer("모델");
-////        Answer answer3 = Answer.createAnswer("스프링부트");
-////        Answer answer4 = Answer.createAnswer("클라우드");
-////        Answer answer5 = Answer.createAnswer("배치");
-////
-////        Problem problem = Problem.createProblem("스프링이 무엇인가?",
-////                category, user, problemPicture, answer1, answer2, answer3, answer4, answer5);
-////        em.persist(problem);
-////
-////        Problem problem2 = Problem.createProblem("REST API란?",
-////                category, user, problemPicture, answer3, answer4, answer5);
-////        em.persist(problem2);
-////
-////        em.flush();
-////        em.clear();
-////
-////        List<Problem> waitList = problemRepository.findByStatus(ProblemStatus.wait);
-////        for (Problem p: waitList) {
-////            System.out.println(p);
-////        }
-////    }
-////
 //    @Test
 //    @Rollback(false)
-//    void updateProblem(){
-//        List<Problem> one = problemRepository.findAll();
-//        for(Problem p: one){
-//            p.setStatus(ProblemStatus.accept);
-//        }
+//    void findByStatus() {
+//        Category category = new Category("스프링", "스프링이 무엇일까");
+//        em.persist(category);
+//
+//        User user = new User("sgs1159@naver.com", "순조에용", "이조순", "1234", "팀원", "유저디폴트이미지");
+//        em.persist(user);
+//
+//        ProblemPicture problemPicture = new ProblemPicture("디폴트이미지 url");
+//        Answer answer1 = Answer.createAnswer("mvc");
+//        Answer answer2 = Answer.createAnswer("모델");
+//        Answer answer3 = Answer.createAnswer("스프링부트");
+//        Answer answer4 = Answer.createAnswer("클라우드");
+//        Answer answer5 = Answer.createAnswer("배치");
+//
+//        Problem problem = Problem.createProblem("스프링이 무엇인가?",
+//                category, user, problemPicture, answer1, answer2, answer3, answer4, answer5);
+//        em.persist(problem);
+//
+//        Problem problem2 = Problem.createProblem("REST API란?",
+//                category, user, problemPicture, answer3, answer4, answer5);
+//        em.persist(problem2);
+//
 //        em.flush();
 //        em.clear();
 //
+//        List<Problem> waitList = problemRepository.findByStatus(ProblemStatus.wait);
+//        for (Problem p: waitList) {
+//            System.out.println(p);
+//        }
 //    }
 //
-//}
+    @Test
+    @Rollback(false)
+    void updateProblem(){
+        List<Problem> one = problemRepository.findAll();
+        for(Problem p: one){
+            p.setStatus(ProblemStatus.accept);
+        }
+        em.flush();
+        em.clear();
+
+    }
+
+}
