6dd520f6c970911ad41922310be3a27c83e56325
xrl0603 <xrl0603@naver.com>
2021-11-01 14:15:47 +0900

    [CS] feat : web speech api 기본 구성 설정

--- frontend/src/views/logout-cover/rightpage.vue
+++ frontend/src/views/logout-cover/rightpage.vue
@@ -5,11 +5,53 @@
       <div>개발자를 위한 CS 역량 강화</div>
       <div class="cstitle">CS의 정석</div>
     </div>
+    <div class="voice">
+      <q-btn
+        class="speech-to-text"
+        flat
+        style="color: #fc3f2d"
+        @click="startSpeechToTxt"
+        >Speech to text</q-btn
+      >
+      <p>{{ transcription_ }}</p>
+    </div>
   </div>
 </template>
 <script>
 export default {
   name: "logout-cover-right",
+  data() {
+    return {
+      runtimeTranscription_: "",
+      transcription_: [],
+      lang_: "ko-KR",
+    };
+  },
+  methods: {
+    startSpeechToTxt() {
+      console.log("start text");
+      // initialisation of voicereco
+      const recognition = new window.SpeechRecognition();
+      recognition.lang = this.lang_;
+      recognition.interimResults = true;
+
+      // event current voice reco word
+      recognition.addEventListener("result", (event) => {
+        var text = Array.from(event.results)
+          .map((result) => result[0])
+          .map((result) => result.transcript)
+          .join("");
+        this.runtimeTranscription_ = text;
+      });
+      // end of transcription
+      recognition.addEventListener("end", () => {
+        this.transcription_.push(this.runtimeTranscription_);
+        this.runtimeTranscription_ = "";
+        recognition.stop();
+      });
+      recognition.start();
+    },
+  },
 };
 </script>
 <style lang=""></style>
