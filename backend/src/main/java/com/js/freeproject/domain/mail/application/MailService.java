67e142139089ecedf4ec4a8a5d73ee3ded0eddf8
Seojeonguk <uk7880@naver.com>
2021-11-02 13:49:41 +0900

    [BE] refactor : Move getcontent function

--- backend/src/main/java/com/js/freeproject/domain/mail/application/MailService.java
+++ backend/src/main/java/com/js/freeproject/domain/mail/application/MailService.java
@@ -1,5 +1,9 @@
 package com.js.freeproject.domain.mail.application;
 
+import java.util.UUID;
+
+import javax.mail.MessagingException;
+
 import org.springframework.stereotype.Service;
 
 import com.js.freeproject.domain.mail.domain.Mail;
@@ -12,40 +16,18 @@ import lombok.RequiredArgsConstructor;
 public class MailService {
 	private final MailUtil mailUtil;
 	
-	public void sendMail(String to, String type) throws Exception {
-		String title = "SCPC 이메일 인증입니다.";
+	public void sendMail(String to,String type) throws MessagingException  {
+		String key = UUID.randomUUID().toString();
+		
 		Mail mail = Mail.builder()
-				.title(title)
+				.title("SCPC 이메일 인증입니다.")
 				.to(to)
-				.content(getcontent(type))
+				.content(mailUtil.getcontent(type,key))
 				.build();
-		System.out.println(mail.toString());
-		try {
-			mailUtil.SendMail(mail);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
+		mailUtil.SendMail(mail);
 	}
 	
-	public String getcontent(String type) {
-		StringBuilder content = new StringBuilder();
-		content.append("<!DOCTYPE html>");
-		content.append("<html>");
-		content.append("<head></head>");
-		content.append("<body>");
-		content.append("<div>");
-		if(type.equals("pass")) {
-			content.append("비밀번호 찾기 전송");
-			content.append("<a href='http://localhost'>비밀번호 찾기 링크</a>");
-		} else if(type.equals("mail")) {
-			content.append("메일 인증 전송");
-			content.append("<a href='http://localhost'>메일 인증 링크</a>");
-		}
-		content.append("</div>");
-		content.append("</body>");
-		content.append("</html>");
-		return content.toString();
-	}
+	
 }
 
--- backend/src/main/java/com/js/freeproject/global/util/MailUtil.java
+++ backend/src/main/java/com/js/freeproject/global/util/MailUtil.java
@@ -1,5 +1,6 @@
 package com.js.freeproject.global.util;
 
+import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.javamail.JavaMailSender;
@@ -15,16 +16,34 @@ import lombok.RequiredArgsConstructor;
 public class MailUtil {
 	private final JavaMailSender javaMailSender;
 	
-	public void SendMail(Mail mail) throws Exception {
+	public void SendMail(Mail mail) throws MessagingException {
 		MimeMessage message = javaMailSender.createMimeMessage();
 		MimeMessageHelper messageHelper = new MimeMessageHelper(message);
-		System.out.println("-----------------------------");
-		System.out.println(mail.toString());
 		messageHelper.setTo(mail.getTo());
 		messageHelper.setFrom("CSPS");
 		messageHelper.setSubject(mail.getTitle());
 		messageHelper.setText(mail.getContent(),true);
 		
 		javaMailSender.send(message);
+	}
+	
+	public String getcontent(String type,String key) {
+		StringBuilder content = new StringBuilder();
+		content.append("<!DOCTYPE html>");
+		content.append("<html>");
+		content.append("<head></head>");
+		content.append("<body>");
+		content.append("<div>");
+		if(type.equals("pass")) {
+			content.append("비밀번호 찾기 전송");
+			content.append("<a href='http://localhost:8080/user/fixpass?key="+key + "'>비밀번호 찾기 링크</a>");
+		} else if(type.equals("mail")) {
+			content.append("메일 인증 전송");
+			content.append("<a href='http://localhost'>메일 인증 링크</a>");
+		}
+		content.append("</div>");
+		content.append("</body>");
+		content.append("</html>");
+		return content.toString();
 	}
 }
